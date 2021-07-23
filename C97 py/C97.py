b81c3aca88c57e0807ec95b4ca5385ec36cf7aff
dheerajyer2418 <69052518+dheerajyer2418@users.noreply.github.com>
2021-07-22 21:51:39 -0500

    Add files via upload

---
+++ C97 py/C97.py
@@ -0,0 +1,14 @@
+#myName = "Dheeraj"
+#myAge = 15
+#print(myName)
+#print(myAge)
+#myFriendList = ["Asrith","Ajay","Srikumar"]
+#print(myFriendList[2])
+#pocketMoney = input("Enter your pocket money: ")
+#print(pocketMoney)
+#for friend in myFriendList:
+   # print(friend)
+count = 5
+while(count >= 0):
+    print(count)
+    count = count-1
